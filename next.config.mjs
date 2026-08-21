6da44e47d8f1bb881b0da3283c9e1d75e702a829
jimmylarter <jimmylarterdesign@gmail.com>
2026-08-21 16:06:34 +1000

    Update Next.js configuration for export and base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  basePath: '/Otix-Studio',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
